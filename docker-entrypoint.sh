55a029ab2858a93f38d0ee0ca36856fa27693ebd
Chahine Aouled Amor <chahineaouledamor721@gmail.com>
2026-05-08 21:52:35 +0100

    Final

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,5 @@ set -e
 php bin/console cache:clear --no-warmup
 php bin/console cache:warmup
 
-# For fresh database - force schema creation instead of migrations
-php bin/console doctrine:schema:update --force --complete
-
 # Start Apache
 exec "$@"
